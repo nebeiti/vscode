e2259f7bb253e3e9f36325d615a17a83828b1b30
nebiti <nebeyu8124@gmail.com>
2024-09-15 16:13:39 +0300

    5.7.1

--- try/hi.js
+++
@@ -1,8 +0,0 @@
-function openLightbox(src) {
-    document.getElementById('lightbox-img').src = src;
-    document.getElementById('lightbox').style.display = 'flex';
-}
-
-function closeLightbox() {
-    document.getElementById('lightbox').style.display = 'none';
-}
