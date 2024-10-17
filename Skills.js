bde57cf63db031a402dc445ba6ee6875d68b41c8
nebiti <nebeyu8124@gmail.com>
2024-10-17 21:10:14 +0300

    5.9.7

--- Skills.js
+++ Skills.js
@@ -1,4 +1,18 @@
-( function($) {
+document.addEventListener("DOMContentLoaded", function() {
+  const menuIcon = document.getElementById('menu-icon');
+  const navMenu = document.getElementById('nav-menu');
+
+  menuIcon.onclick = function(event) {
+      event.stopPropagation();
+      navMenu.classList.toggle('active'); 
+  };
+
+  document.addEventListener('click', function(event) {
+      if (!navMenu.contains(event.target) && !menuIcon.contains(event.target)) {
+          navMenu.classList.remove('active'); 
+      }
+  });
+});( function($) {
   
     $(document).ready(function() {
             
